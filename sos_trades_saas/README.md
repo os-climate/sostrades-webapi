54295010b03f8b5772db6c904a671624ddca19ab
THEVENET Florent <NG61192@eu.airbus.corp>
2022-02-08 10:05:57 +0100

    Add authentication

--- sos_trades_saas/README.md
+++ sos_trades_saas/README.md
@@ -1,17 +1,55 @@
-# SoSTrades_SAAS
+# SoSTrades SAAS
 
 
 ## Purpose
 
-This API is designed to offer some acces point.
-TBC...
+This API is designed to offer some access point for command line requests.
+
 
 ## Exposed routes
 
+### Authentication
+
+
+    https://<host>/saas/login
+
+
+- POST parameters : 
+  - *username* : user name 
+  - *password* : user password 
+
+- response content :
+  - *access_token* : bearer access token
+
+
 ### GUI study link
 
-1/ url 
-2/ response content
-3/ misc
+
+    https://<host>/saas/study-case-link/<study_id>
+
+
+- GET parameters : 
+  - *study_id* : study identifier (integer) 
+
+- response content :
+  - *study_url* : study gui link
+
+### Load a study
+
+
+    https://<host>/saas/load-study-case/<study_id>
+
+
+- GET parameters : 
+  - *study_id* : study identifier (integer) 
+
+- response content :
+  - *missing_key_here* : loaded study content 
+
+- loaded study content description :
+  - *treenode* : 
+  - *etc...* :
+
+
 
 
--- sos_trades_saas/routes/saas_module.py
+++ sos_trades_saas/routes/saas_module.py
@@ -14,16 +14,51 @@ See the License for the specific language governing permissions and
 limitations under the License.
 '''
 
-from flask import Response, request, make_response, jsonify, abort
+from flask import request, make_response, jsonify, abort
 
+from sos_trades_api.controllers.sostrades_data.authentication_controller import authenticate_user_standard
+from sos_trades_api.tools.authentication.authentication import AuthenticationError, auth_required
 from sos_trades_api.controllers.sostrades_main.study_case_controller import light_load_study_case, load_study_case
-
 from sos_trades_api.base_server import app, study_case_cache
 
 # TODO user / group credentials
+# TODO optional timeout on  *load_study*
+
+
+@app.route(f'/saas/login', methods=['POST'])
+def login():
+    """
+    Return bearer access token
+
+    @return: json response like {'access_token': access_token}
+    """
+
+    if request.json is None:
+        abort(400, "'username' and 'password' not found in request")
+
+    elif request.json.get('username') is None:
+        abort(400, "'username' not found in request")
+
+    elif request.json.get('password') is None:
+        abort(400, "'password' not found in request")
+
+    try:
+        username = request.json.get('username')
+        password = request.json.get('password')
+
+        access_token, _, _, _ = authenticate_user_standard(username, password)
+
+        return make_response(jsonify({'access_token': access_token}), 200)
+
+    except AuthenticationError as error:
+        abort(403, str(error))
+
+    except Exception as err:
+        abort(400, str(err))
 
 
 @app.route(f'/saas/study-case-link/<int:study_id>', methods=['GET'])
+@auth_required
 def get_study_case_link(study_id: int):
     """
     Return study-case web-GUI url
@@ -33,31 +68,23 @@ def get_study_case_link(study_id: int):
 
     if request.method == "GET":
 
-        study_url = ""
-        msg = ""
-        status_code = 200
-
         try:
+
             study_case_cache.get_study_case(study_id, False)
             study_url = f'{app.config.get("SOS_TRADES_FRONT_END_DNS", "")}study/{study_id}'
 
+            return make_response(jsonify({"study_url": study_url}), 200)
+
         except Exception as e:
-            msg = str(e)
-            status_code = 400
-
-        finally:
-            payload = {
-                "study_url": study_url,
-                "message": msg
-            }
-
-        return make_response(jsonify(payload), status_code)
+            abort(400, str(e))
 
     abort(405)
 
 
 @app.route(f'/saas/load-study-case/<int:study_id>', methods=['GET'])
-def load_study(study_id: int):
+@app.route(f'/saas/load-study-case/<int:study_id>/<int:timeout>', methods=['GET'])
+@auth_required
+def load_study(study_id: int, timeout: int = 30):
     """
     Return
 
@@ -68,6 +95,7 @@ def load_study(study_id: int):
 
         msg = ""
         status_code = 200
+        print(timeout)
 
         try:
             # do stuff here
