c263367279c0a438affe06e3b7bdce45dfaf2b21
THEVENET Florent <NG61192@eu.airbus.corp>
2022-02-11 15:32:24 +0100

    Add monitor route

--- sos_trades_saas/routes/saas_module.py
+++ sos_trades_saas/routes/saas_module.py
@@ -18,6 +18,7 @@ limitations under the License.
 import time
 from flask import request, make_response, jsonify, abort
 
+from sos_trades_api.controllers.sostrades_data.calculation_controller import calculation_status
 from sos_trades_api.models.database_models import AccessRights
 from sos_trades_api.controllers.sostrades_data.authentication_controller import authenticate_user_standard
 from sos_trades_api.tools.authentication.authentication import AuthenticationError, auth_required,\
@@ -27,6 +28,7 @@ from sos_trades_api.controllers.sostrades_main.study_case_controller import ligh
 from sos_trades_saas.controller.saas_module_controller import filter_tree_node_data, filter_children_data
 
 from sos_trades_api.base_server import app, study_case_cache
+from sos_trades_saas.tools.credentials import restricted_viewer_required
 
 
 @app.route(f'/saas/login', methods=['POST'])
@@ -88,6 +90,7 @@ def get_study_case_link(study_id: int):
 @app.route(f'/saas/load-study-case/<int:study_id>', methods=['GET'])
 @app.route(f'/saas/load-study-case/<int:study_id>/<int:timeout>', methods=['GET'])
 @auth_required
+@restricted_viewer_required
 def load_study(study_id: int, timeout: int = 30):
     """
     Return dictionary instance containing loaded study
@@ -100,24 +103,25 @@ def load_study(study_id: int, timeout: int = 30):
     """
 
     if request.method == "GET":
-
-        user = get_authenticated_user()
-        study_case_access = StudyCaseAccess(user.id)
-        if not study_case_access.check_user_right_for_study(AccessRights.RESTRICTED_VIEWER, study_id):
-            raise BadRequest('You do not have the necessary rights to load this study case')
-        study_access_right = study_case_access.get_user_right_for_study(study_id)
-
         try:
+
+            user = get_authenticated_user()
+            study_case_access = StudyCaseAccess(user.id)
+            study_access_right = study_case_access.get_user_right_for_study(study_id)
+
             loaded_study = load_study_case(study_id, study_access_right, user.id)
             for _ in range(timeout):
+
                 study_manager = light_load_study_case(study_id)
+
                 if not study_manager.loaded:
-                    time.sleep(3)
+                    time.sleep(1)
                     loaded_study = load_study_case(study_id, study_access_right, user.id)
                 else:
                     break
 
             tree_node = loaded_study.treenode
+            status_code = 200
 
             payload = {
                 "study_name": loaded_study.study_case.name,
@@ -133,3 +137,16 @@ def load_study(study_id: int, timeout: int = 30):
 
         return make_response(jsonify(payload), status_code)
     abort(405)
+
+
+@app.route(f'/saas/monitor/study-case/<int:study_id>', methods=['GET'])
+@auth_required
+@restricted_viewer_required
+def monitor_study_execution(study_id: int):
+    try:
+
+        return make_response(jsonify(calculation_status(study_id)), 200)
+
+    except Exception as e:
+        abort(400, str(e))
+
