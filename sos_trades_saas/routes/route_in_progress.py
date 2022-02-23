536e455016c51b53fea9b9e75dc9da3877fb025f
THEVENET Florent <NG61192@eu.airbus.corp>
2022-02-23 14:18:28 +0100

    Update route_in_progress.py

--- sos_trades_saas/routes/route_in_progress.py
+++ sos_trades_saas/routes/route_in_progress.py
@@ -112,4 +112,3 @@ def postprocess(study_id):
     study_manager = light_load_study_case(study_id)
     execute_calculation(study_id, "user_test")
     return Response({"hello": "hello you"}, status=200)
-
