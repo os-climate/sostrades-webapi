4a79e8addf6bc601cc6ff50e2aa9042a88ea476a
Marie-Audrey Gueylard <marie-audrey.gueylard.external@airbus.com>
2022-04-26 16:03:45 +0200

    remove prints in init_process

--- sos_trades_api/base_server.py
+++ sos_trades_api/base_server.py
@@ -138,8 +138,6 @@ def database_process_setup():
             # user manager
             group_manager_account_account = Group.query.filter(
                 Group.is_default_applicative_group).first()
-            print(f'default group:')
-            print(group_manager_account_account)
             if group_manager_account_account is None:
                 group_manager_account_account = Group.query.filter(
                 Group.name == Group.SOS_TRADES_DEV_GROUP).first()
