3adf4e6b294ee8a1db0b6b94ea66a5073759d0c6
geoffreydelric <geoffrey.delric.external@airbus.com>
2022-06-29 16:38:44 +0200

    add sort for process list

--- sos_trades_api/controllers/sostrades_data/process_controller.py
+++ sos_trades_api/controllers/sostrades_data/process_controller.py
@@ -47,46 +47,51 @@ def api_get_processes_for_user(user: User) -> List[LoadedProcess]:
 
     :param user: user for which process has to be filtered
     """
-
+    result = []
     process_access = ProcessAccess(user_id=user.id)
     authorized_process_list = process_access.get_authorized_process()
 
-    apply_ontology_to_loaded_process(authorized_process_list)
-
-    # Adding reference list
-    all_references = ReferenceStudy.query.filter(ReferenceStudy.execution_status == ReferenceStudy.FINISHED).all()
-
-    for authorized_process in authorized_process_list:
-        process_references = list(
-            filter(lambda ref_process: ref_process.process_id == authorized_process.id, all_references))
-        process_ref_list = []
-        for ref in process_references:
-            new_ref = StudyCaseDto()
-            new_ref.name = ref.reference_path.split(".")[-1]
-            new_ref.process_display_name = authorized_process.process_name
-            new_ref.repository_display_name = authorized_process.repository_name
-            new_ref.process = authorized_process.process_id
-            new_ref.repository = authorized_process.repository_id
-            new_ref.creation_date = ref.creation_date
-            new_ref.group_id = None
-            new_ref.group_name = 'All groups'
-
-            if ref.reference_type == ReferenceStudy.TYPE_REFERENCE:
-                new_ref.description = 'Reference study'
-                new_ref.study_type = 'Reference'
-            else:
-                new_ref.description = 'Usecase data'
-                new_ref.study_type = 'UsecaseData'
-            process_ref_list.append(new_ref)
-
-        if len(process_ref_list) > 0:
-            authorized_process.reference_list = process_ref_list
-
-    # Sort by process display name
     if len(authorized_process_list) > 0:
+
+        apply_ontology_to_loaded_process(authorized_process_list)
+
+        # Sort by process display name
         authorized_process_list = sorted(
             authorized_process_list, key=lambda res: res.process_name.lower())
-    return authorized_process_list
+
+        # Adding reference list
+        all_references = ReferenceStudy.query.filter(ReferenceStudy.execution_status == ReferenceStudy.FINISHED).all()
+
+        for authorized_process in authorized_process_list:
+            process_references = list(
+                filter(lambda ref_process: ref_process.process_id == authorized_process.id, all_references))
+            process_ref_list = []
+            for ref in process_references:
+                new_ref = StudyCaseDto()
+                new_ref.name = ref.reference_path.split(".")[-1]
+                new_ref.process_display_name = authorized_process.process_name
+                new_ref.repository_display_name = authorized_process.repository_name
+                new_ref.process = authorized_process.process_id
+                new_ref.repository = authorized_process.repository_id
+                new_ref.creation_date = ref.creation_date
+                new_ref.group_id = None
+                new_ref.group_name = 'All groups'
+
+                if ref.reference_type == ReferenceStudy.TYPE_REFERENCE:
+                    new_ref.description = 'Reference study'
+                    new_ref.study_type = 'Reference'
+                else:
+                    new_ref.description = 'Usecase data'
+                    new_ref.study_type = 'UsecaseData'
+                process_ref_list.append(new_ref)
+
+            if len(process_ref_list) > 0:
+                authorized_process.reference_list = process_ref_list
+
+        result = sorted(
+            authorized_process_list, key=lambda res: res.is_manager or res.is_contributor, reverse=True)
+
+    return result
 
 
 def api_get_processes_for_dashboard() -> List[LoadedProcess]:
