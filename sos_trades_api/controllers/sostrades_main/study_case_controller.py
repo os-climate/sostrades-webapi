ba5e0a4b6a08d70cd9468e8632b70def4284c865
Marie-Audrey Gueylard <marie-audrey.gueylard.external@airbus.com>
2022-09-29 10:35:46 +0200

    fix import error jsonify

--- sos_trades_api/controllers/sostrades_main/study_case_controller.py
+++ sos_trades_api/controllers/sostrades_main/study_case_controller.py
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
-
+from flask import jsonify
 
 """
 mode: python; py-indent-offset: 4; tab-width: 4; coding: utf-8
