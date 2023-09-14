9af891504f150309676eb2c1665374c97c3e5443
geo31200 <Geoffrey.delric@capgemini.com>
2023-09-14 15:27:57 +0200

    redirection with Github authentication

--- sos_trades_api/routes/data/authentication_module.py
+++ sos_trades_api/routes/data/authentication_module.py
@@ -212,12 +212,13 @@ def github_oauth_is_available():
     return make_response(jsonify(github_settings.is_available), 200)
 
 
-@app.route(f'/api/data/github/oauth/authorize', methods=['GET'])
+@app.route(f'/api/data/github/oauth/authorize', methods=['POST'])
 def github_oauth_authorize():
 
     github_settings = GitHubSettings()
 
     github_oauth_url = ''
+    uri = request.json.get('redirect_uri', None)
 
     if github_settings.is_available:
 
@@ -227,6 +228,9 @@ def github_oauth_authorize():
             'state': GitHubSettings.get_state(),
             'allow_signup': 'true'
         }
+        if uri != '':
+            params['redirect_uri'] = uri
+            
         github_oauth_url = furl(github_settings.authorize_url).set(params).url
 
     return make_response(jsonify(github_oauth_url), 200)
