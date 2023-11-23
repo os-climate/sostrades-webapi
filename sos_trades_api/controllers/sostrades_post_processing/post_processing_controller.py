9e046e8920cd804aa789e06c3a07242dc1b18240
ggoyon <guillaume.goyon@capgemini.com>
2023-11-23 11:01:18 +0100

    Updated headers

--- sos_trades_api/controllers/sostrades_post_processing/post_processing_controller.py
+++ sos_trades_api/controllers/sostrades_post_processing/post_processing_controller.py
@@ -1,12 +1,19 @@
-"""
-mode: python; py-indent-offset: 4; tab-width: 4; coding: utf-8
-Copyright (C) 2020 Airbus SAS
-Created on March, 2020
+'''
+Copyright 2022 Airbus SAS
+Modifications on 2023/11/23 Copyright 2023 Capgemini
 
-@author: NG8B2DB
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-post processing Functions
-"""
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
 
 from sos_trades_api.server.base_server import study_case_cache, app
 from sostrades_core.tools.post_processing.post_processing_factory import PostProcessingFactory
