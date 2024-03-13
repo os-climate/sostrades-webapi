88f919fd2abb351974b7ec70a0d77627e3731963
gdelric-capgemini <Geoffrey.delric@capgemini.com>
2024-03-13 09:06:57 +0100

    update get_shared_study when allocation is None

--- sos_trades_api/controllers/sostrades_data/study_case_controller.py
+++ sos_trades_api/controllers/sostrades_data/study_case_controller.py
@@ -509,23 +509,14 @@ def get_user_shared_study_case(user_identifier: int):
             if user_study.creation_status != StudyCase.CREATION_DONE:
                 allocation = get_study_case_allocation(user_study.id)
                 # deal with error cases:
-                if allocation is None:
-                    app.logger.error(f"The study '{user_study.id}' has not an allocation but his status is not done")
-                else:
-                    if allocation.status != StudyCaseAllocation.DONE and user_study.creation_status == StudyCase.CREATION_IN_PROGRESS:
+                if allocation is None or (allocation.status != StudyCaseAllocation.DONE and user_study.creation_status == StudyCase.CREATION_IN_PROGRESS):
+                    user_study.creation_status = StudyCase.CREATION_ERROR
+                    user_study.error = "An error occured while creation, please reload the study to finalize the creation"
+                elif allocation.status == StudyCaseAllocation.PENDING:
+                    if datetime.now() - allocation.creation_date > timedelta(minutes=1):
+                        app.logger.info(f"time for loading study pod: {datetime.now() - allocation.creation_date}")
                         user_study.creation_status = StudyCase.CREATION_ERROR
-                        user_study.error = "An error occured while creation, please reload the study to finalize the creation"
-                    elif allocation.status == StudyCaseAllocation.PENDING:
-                        if datetime.now() - allocation.creation_date > timedelta(minutes=1):
-                            app.logger.info(f"time for loading study pod: {datetime.now() - allocation.creation_date}")
-                            user_study.creation_status = StudyCase.CREATION_ERROR
-                            user_study.error = "Waiting for a study pod to end the creation of the study, may need to be reloaded"
-
-                
-                
-                    
-
-                
+                        user_study.error = "Waiting for a study pod to end the creation of the study, may need to be reloaded"
 
         process_metadata = load_processes_metadata(processes_metadata)
         repository_metadata = load_repositories_metadata(repositories_metadata)
