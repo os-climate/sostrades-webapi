6167789371ff8c21df8bd4a3990c0ca4cce903f7
magueylard <marie-audrey.gueylard@capgemini.com>
2024-04-18 14:45:35 +0200

    update pod and execution status

--- sos_trades_api/tools/execution/execution_tools.py
+++ sos_trades_api/tools/execution/execution_tools.py
@@ -48,9 +48,12 @@ def update_study_case_execution_status(study_case_execution: StudyCaseExecution)
                 study_case_execution.message = f'Pod is loading : {pod_allocation.message}'
             else:
                 study_case_execution.message = f'Pod is loading'
-
+        elif pod_allocation.pod_status == PodAllocation.RUNNING and study_case_execution.execution_status == StudyCaseExecution.POD_PENDING:
+            study_case_execution.message = 'Pod is up, computation should start soon'
+            study_case_execution.execution_status = StudyCaseExecution.PENDING
         elif pod_allocation.pod_status == PodAllocation.RUNNING:
             study_case_execution.execution_status = StudyCaseExecution.RUNNING
+            study_case_execution.execution_status = ''
 
         db.session.add(study_case_execution)
         db.session.commit()
