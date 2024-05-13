c3df7f5d8ae34624083a0dd419ca57c7b628fdee
magueylard <marie-audrey.gueylard@capgemini.com>
2024-05-13 10:22:25 +0200

    fix l0 calculation

--- sos_trades_api/tests/controllers/l0_test_calculation.py
+++ sos_trades_api/tests/controllers/l0_test_calculation.py
@@ -150,11 +150,11 @@ class TestCalculation(DatabaseUnitTestConfiguration):
                           'Study case execution status not coherent')
             
             #check allocation creation:
-            allocations = PodAllocation.query.filter(PodAllocation.identifier == sc.current_execution_id).filter(
+            allocations = PodAllocation.query.filter(PodAllocation.identifier == sc.id).filter(
                                                     PodAllocation.pod_type == PodAllocation.TYPE_EXECUTION
                                                     ).all()
-            #TODO: set this line when the pb is fixed
-            # self.assertTrue(len(allocations) == 1, 'There is more than one allocation for this execution')
+
+            self.assertTrue(len(allocations) == 1, 'There is more than one allocation for this execution')
             self.assertIsNotNone(allocations[0], 'Allocation not found')
             self.assertEqual(allocations[0].pod_status, PodAllocation.RUNNING,'Allocation has not the Running status')
 
