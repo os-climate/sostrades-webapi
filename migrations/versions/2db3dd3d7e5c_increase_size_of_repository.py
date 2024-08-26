a43d5d9b442af91eda875d30d7d091fec3db8a6b
magueylard <marie-audrey.gueylard@capgemini.com>
2024-08-26 18:18:23 +0200

    prevent saving heavy data in DB at parameter change

--- migrations/versions/2db3dd3d7e5c_increase_size_of_repository.py
+++ migrations/versions/2db3dd3d7e5c_increase_size_of_repository.py
@@ -5,8 +5,8 @@ Revises: 6742a7abbc12
 Create Date: 2024-08-08 18:43:41.524297
 
 """
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects import mysql
 
 # revision identifiers, used by Alembic.
