c07e31ffe9d849585c3af3a161d84b051b6560ca
magueylard <marie-audrey.gueylard@capgemini.com>
2024-09-06 14:52:13 +0200

    Add new route to check study is active and remove route for watcher

--- sos_trades_api/server/base_server.py
+++ sos_trades_api/server/base_server.py
@@ -1009,22 +1009,5 @@ if app.config["ENVIRONMENT"] != UNIT_TEST:
 def ping():
     return make_response(jsonify("pong"), 200)
 
-from sos_trades_api.tools.authentication.authentication import local_only
 
 
-@app.route("/api/update-allocation-loop", methods=["GET"])
-@local_only
-def update_allocation_loop():
-    '''
-    start a thread to check allocation pods status, route only accessible on local
-    '''
-    import threading
-
-    from flask import jsonify, make_response
-
-    from sos_trades_api.server.base_server import update_all_pod_status_loop_method
-
-    threading.Thread(
-                 target=update_all_pod_status_loop_method
-                ).start()
-    return make_response(jsonify("allocation pod watcher started"), 200)
