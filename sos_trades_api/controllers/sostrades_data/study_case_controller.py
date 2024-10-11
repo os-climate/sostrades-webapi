f3d18751b4f7bca7479bbd6450dddf7e2dc0eba3
magueylard <marie-audrey.gueylard@capgemini.com>
2024-10-11 17:11:24 +0200

    update pod ressource error message

--- sos_trades_api/controllers/sostrades_data/study_case_controller.py
+++ sos_trades_api/controllers/sostrades_data/study_case_controller.py
@@ -740,7 +740,7 @@ def add_study_information_on_status(user_study: StudyCase):
             user_study.creation_status = StudyCase.CREATION_ERROR
             if allocation is not None:
                 if allocation.pod_status == PodAllocation.OOMKILLED:
-                    user_study.error = "An error occured while creation, pod had not enough resources, you may need to choose a bigger pod size before reloading the study to finalize the creation"
+                    user_study.error = f"An error occured while creation, pod (actual pod size {allocation.flavor}) had not enough resources, you may need to choose a bigger pod size before reloading the study to finalize the creation"
                 else:
                     user_study.error = "An error occured while creation, please reload the study to finalize the creation"
 
