74d450d567785f62abf210682bb4f941d93785c1
magueylard <marie-audrey.gueylard@capgemini.com>
2024-10-25 14:58:59 +0200

    add a test to reload

---
+++ sos_trades_api/tests/controllers/test_memory.py
@@ -0,0 +1,86 @@
+
+import argparse
+import os
+from os.path import dirname, join
+
+from dotenv import load_dotenv
+if os.environ.get("SOS_TRADES_SERVER_CONFIGURATION") is None:
+    dotenv_path = join(dirname(__file__),"..", "..", "..", ".flaskenv")
+    print(dotenv_path)
+    load_dotenv(dotenv_path)
+
+
+from sos_trades_api.server.base_server import app
+from memory_profiler import memory_usage
+from sos_trades_api.controllers.sostrades_main.study_case_controller import (
+    load_study_case,
+)
+from sos_trades_api.models.database_models import AccessRights, StudyCase
+from sos_trades_api.models.loaded_study_case import LoadStatus
+from sos_trades_api.server.base_server import study_case_cache
+from time import sleep
+
+
+
+def test_reload_study_case(study_id, user_id ):
+        
+
+    with app.app_context():
+        study_test = StudyCase.query.filter(StudyCase.id == study_id).first()
+        
+        study_manager = study_case_cache.get_study_case(study_test.id, False)
+
+        mem_before = memory_usage()[0]
+        app.logger.info(f"Memory before first loading: {mem_before} MB")
+        loaded_study = load_study_case(
+            study_test.id, AccessRights.MANAGER, user_id)
+
+        #  wait until study was updated (thread behind)
+        stop = False
+        counter = 0
+
+        while not stop:
+            if study_manager.load_status == LoadStatus.LOADED:
+                stop = True
+            else:
+                if counter > 360:
+                    raise Exception( "test_update_study_parameters update study parameter too long, check thread")
+                counter = counter + 1
+                sleep(1)
+        
+        # reload 10 times
+        for i in range(0,10):
+            mem_after = memory_usage()[0]
+            app.logger.info(f"Memory before {i} reloading: {mem_after} MB")
+            loaded_study = load_study_case(
+                study_test.id, AccessRights.MANAGER, user_id, reload=True)
+            
+            while not stop:
+                if study_manager.load_status == LoadStatus.LOADED:
+                    stop = True
+                else:
+                    if counter > 360:
+                        raise Exception("test_update_study_parameters update study parameter too long, check thread")
+                    counter = counter + 1
+                    sleep(1)
+            mem_after_reload = memory_usage()[0]
+            app.logger.info(f"Memory after reload loading: {mem_after_reload} MB")
+            app.logger.info(f"Memory used by this block: {mem_after_reload - mem_after} MB")
+            sleep(10)
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='test memory')
+
+    parser.add_argument(
+        '--user_id',
+        nargs='?',
+        type=int
+    )
+
+    parser.add_argument(
+        '--study_id', nargs='?', type=int, help='Generate the given reference'
+    )
+
+    args = vars(parser.parse_args())
+
+    test_reload_study_case(args['study_id'],args['user_id'])
